a65f775c07ba89e95a653be3ad89186665f25e65
pjotr.brunain <pjotr.brunain@student.odisee.be>
2024-12-01 20:34:42 +0100

    first commit

---
+++ 2023/AdventOfCode/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        using (var fileStream = File.OpenRead("input.txt"))
+        using (var reader = new StreamReader(fileStream, System.Text.Encoding.UTF8))
+        {
+            DayOnePartTwo(reader);
+        }
+    }
+
+    static void DayOnePartOne(StreamReader reader)
+    {
+        char[] numbers = "0123456789".ToCharArray();
+
+        string line;
+        int total = 0;
+        while((line = reader.ReadLine()) != null)
+        {
+            var lineArray = line.ToCharArray();
+            string? first = null;
+            string? last = null;
+
+            ReplaceStringNumWithActualNum(line);
+
+            for (int i = 0; i < lineArray.Length; i++)
+            {
+                char c = lineArray[i];
+                if (numbers.Contains(c))
+                {
+                    last = c.ToString();
+                    first = first != null ? first : c.ToString();
+                }
+            }
+            if (first != null)
+            {
+                string num = first + last;
+                total += int.Parse(num);
+            }
+        }
+        Console.WriteLine(total.ToString());
+    }
+
+    private static string ReplaceStringNumWithActualNum(string line)
+    {
+        string[] numbersAsStrings = new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        for (int i = 0; i < numbersAsStrings.Length; i++)
+        {
+            line = Regex.Replace(line, numbersAsStrings[i], i.ToString());
+        }
+        return line;
+    }
+
+    static void DayOnePartTwo(StreamReader reader)
+    {
+        //string[] matches = new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        string regex = @"zero|one|two|three|four|five|six|seven|eight|nine|\d";
+        string line;
+        int total = 0;
+        while ((line = reader.ReadLine()) != null)
+        {
+            string? first = null;
+            string? last = null;
+
+            var matches = Regex.Matches(line, regex);
+
+            first = int.Parse(ReplaceStringNumWithActualNum(matches.First().Value)).ToString();
+            last = int.Parse(ReplaceStringNumWithActualNum(matches.Last().Value)).ToString();
+
+            if (first != null)
+            {
+                string num = first + last;
+                total += int.Parse(num);
+            }
+        }
+
+        Console.WriteLine(total.ToString());
+    }
+}
---
+++ 2024/AdventOfCode/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        using (var fileStream = File.OpenRead("input.txt"))
+        using (var streamReader = new StreamReader(fileStream, System.Text.Encoding.UTF8))
+        {
+            dayOnePartOne(streamReader);
+        }
+    }
+
+    static void dayOnePartOne(StreamReader streamReader)
+    {
+        List<int> firstColumn = new List<int>();
+        List<int> secondColumn = new List<int>();
+        string line;
+        while((line = streamReader.ReadLine()) != null)
+        {
+            string[] numbers = line.Split("   ");
+            if (numbers.Length > 1)
+            {
+                firstColumn.Add(int.Parse(numbers[0]));
+                secondColumn.Add(int.Parse(numbers[1]));
+            }
+        }
+
+        firstColumn.Sort();
+        secondColumn.Sort();
+        int totalDistance = 0;
+
+        for (int i = 0; i < firstColumn.Count; i++)
+        {
+            int distance = firstColumn[i] - secondColumn[i];
+            distance = Math.Abs(distance);
+            totalDistance += distance;
+        }
+
+        Console.WriteLine(totalDistance);
+
+        dayOnePartTwo(firstColumn, secondColumn);
+    }
+
+    static void dayOnePartTwo(List<int> firstColumn, List<int> secondColumn)
+    {
+        for (int i = 0; i < firstColumn.Count; i++)
+        {
+            int num = firstColumn[i];
+            int count = 0;
+            foreach (int num2 in secondColumn)
+            {
+                if (num == num2) count++;
+            }
+            firstColumn[i] *= count;
+        }
+
+        int sum = firstColumn.Sum();
+        Console.WriteLine(sum);
+    }
+}
