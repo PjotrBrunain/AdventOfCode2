b293a5d82e71ddb0cae3f8015510d49637ba50d5
pjotr.brunain <pjotr.brunain@student.odisee.be>
2024-12-02 13:35:15 +0100

    Day2

--- 2024/AdventOfCode/Program.cs
+++ 2024/AdventOfCode/Program.cs
@@ -1,13 +1,17 @@
-﻿namespace AdventOfCode;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace AdventOfCode;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        using (var fileStream = File.OpenRead("input.txt"))
+        using (var fileStream = File.OpenRead("input2.txt"))
         using (var streamReader = new StreamReader(fileStream, System.Text.Encoding.UTF8))
         {
-            dayOnePartOne(streamReader);
+            dayTwoPartTwo(streamReader);
         }
     }
 
@@ -57,5 +61,99 @@ internal class Program
 
         int sum = firstColumn.Sum();
         Console.WriteLine(sum);
+    }
+
+    static void dayTwoPartOne(StreamReader reader) 
+    {
+        //int safeReports = 0;
+        string line;
+        List<List<int>> safeReports = new List<List<int>>();
+        List<List<int>> badReports = new List<List<int>>();
+        while ((line = reader.ReadLine()) != null)
+        {
+            List<int> numbers = new List<int>();
+            string[] strings = line.Split(' ');
+            foreach (string s in strings) 
+            {
+                numbers.Add(int.Parse(s));
+            }
+            if (numbers.Count != numbers.Distinct().Count()) continue;
+            if (((!numbers.Order().SequenceEqual(numbers)) && (!numbers.OrderDescending().SequenceEqual(numbers)))) continue;
+            bool safe = true;
+            for (int i = 0; i < numbers.Count-1; i++)
+            {
+                if (Math.Abs(numbers[i] - numbers[i+1]) > 3) safe = false;
+            }
+            if (!safe) continue;
+            safeReports.Add(numbers);
+        }
+
+        Console.WriteLine(safeReports.Count);
+    }
+
+    static void dayTwoPartTwo(StreamReader reader)
+    {
+        string line;
+        List<List<int>> safeReports = new List<List<int>>();
+        List<List<int>> badReports = new List<List<int>>();
+        while ((line = reader.ReadLine()) != null)
+        {
+            List<int> numbers = new List<int>();
+            string[] strings = line.Split(' ');
+            foreach (string s in strings)
+            {
+                numbers.Add(int.Parse(s));
+            }
+            if (!isSafe(numbers)) 
+            { 
+                numbers = tryMakeSafe(numbers);
+                if (isSafe(numbers)) safeReports.Add(numbers);
+                continue; 
+            }
+            safeReports.Add(numbers);
+        }
+
+        Console.WriteLine(safeReports.Count);
+    }
+
+    private static List<int> tryMakeSafe(List<int> numbers)
+    {
+        if (numbers.Count < 2) return numbers;
+        bool isIncreasing = numbers[0] < numbers[1];
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            //if (!isIncreasing)
+            //{
+            //    if (numbers[i] < numbers[i + 1] || numbers[i] - numbers[i + 1] > 3)
+            //    {
+            //        numbers.RemoveAt(i); 
+            //        break;
+            //    }
+            //}
+            //else
+            //{
+            //    if (numbers[i] > numbers[i + 1] || numbers[i+1] - numbers[i] > 3)
+            //    {
+            //        numbers.RemoveAt(i); 
+            //        break;
+            //    }
+            //}
+            List<int> trySafeNumbers = numbers.ToList();
+            trySafeNumbers.RemoveAt(i);
+            if (isSafe(trySafeNumbers)) return trySafeNumbers;
+        }
+        return numbers;
+    }
+
+    static bool isSafe(List<int> numbers) 
+    {
+        bool isSafe = true;
+        if (numbers.Count != numbers.Distinct().Count()) { isSafe = false; }
+        if (((!numbers.Order().SequenceEqual(numbers)) && (!numbers.OrderDescending().SequenceEqual(numbers)))) { isSafe = false; }
+        for (int i = 0; i < numbers.Count - 1; i++)
+        {
+            if (Math.Abs(numbers[i] - numbers[i + 1]) > 3) { isSafe = false; break; }
+        }
+        return isSafe;
     }
 }
