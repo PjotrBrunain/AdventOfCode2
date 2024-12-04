9fef8719ac83db4c3281a6b88a1b612ebd8c28d7
pjotr.brunain <pjotr.brunain@student.odisee.be>
2024-12-04 15:31:41 +0100

    dayFour

--- 2024/AdventOfCode/Program.cs
+++ 2024/AdventOfCode/Program.cs
@@ -9,10 +9,10 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        using (var fileStream = File.OpenRead("input3.txt"))
+        using (var fileStream = File.OpenRead("input4.txt"))
         using (var streamReader = new StreamReader(fileStream, System.Text.Encoding.UTF8))
         {
-            dayThreePartTwo(streamReader);
+            dayFourPartTwo(streamReader);
         }
     }
 
@@ -164,6 +164,7 @@ internal class Program
 
     #endregion dayTwo
 
+    #region dayThree
     static void dayThreePartOne(StreamReader reader)
     {
         int total = 0;
@@ -197,4 +198,239 @@ internal class Program
         }
         Console.WriteLine(total);
     }
+
+    #endregion dayThree
+
+    #region dayFour
+
+    enum Direction
+    {
+        Bottom,
+        Top,
+        Right,
+        Left,
+        TopRight,
+        BottomRight,
+        BottomLeft,
+        TopLeft
+    }
+
+    static void dayFourPartOne(StreamReader reader)
+    {
+        List<List<char>> crossword = new List<List<char>>();
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            crossword.Add(line.ToList());
+        }
+
+        int total = 0;
+
+        for (int i = 0; i < crossword.Count; i++)
+        {
+            for (int j = 0; j < crossword[i].Count; j++)
+            {
+                if (crossword[i][j] == 'X')
+                {
+                    string word = "XMAS";
+                    //if (HasRight(crossword, i, j) || HasBottom(crossword, i, j) || HasLeft(crossword, i, j) || HasTop(crossword, i, j) || HasTopRight(crossword, i, j) || HasBottomRight(crossword, i, j) || HasBottomLeft(crossword, i, j) || HosTopLeft(crossword, i, j)) total++;
+                    if (hasWord(crossword, i, j, Direction.Right, word)) total++;
+                    if (hasWord(crossword, i, j, Direction.Left, word)) total++;
+                    if (hasWord(crossword, i, j, Direction.Top, word)) total++;
+                    if (hasWord(crossword, i, j, Direction.Bottom, word)) total++;
+                    if (hasWord(crossword, i, j, Direction.TopRight, word)) total++;
+                    if (hasWord(crossword, i, j, Direction.BottomRight, word)) total++;
+                    if (hasWord(crossword, i, j, Direction.BottomLeft, word)) total++;
+                    if (hasWord(crossword, i, j, Direction.TopLeft, word)) total++;
+                    //if (HasTop(crossword, i, j)) total++;
+                    //if (HasBottom(crossword, i, j)) total++;
+                    //if (HasTopRight(crossword, i, j)) total++;
+                    //if (HasBottomRight(crossword, i, j)) total++;
+                    //if (HasBottomLeft(crossword, i, j)) total++;
+                    //if (HasTopLeft(crossword, i, j)) total++;
+                }
+            }
+        }
+
+        Console.WriteLine(total);
+    }
+
+    private static bool hasWord(List<List<char>> crossword, int row, int column, Direction direction, string word) 
+    {
+        List<char> potentialWord;
+        switch (direction)
+        {
+            case Direction.Bottom:
+                if (row + ( word.Length - 1 ) >= crossword.Count) return false;
+                potentialWord = new List<char>();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    potentialWord.Add(crossword[row + i][column]);
+                }
+                return new String(potentialWord.ToArray()) == word;
+            case Direction.Top:
+                if (row - ( word.Length - 1 ) < 0) return false;
+                potentialWord = new List<char>();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    potentialWord.Add(crossword[row - i][column]);
+                }
+                return new String(potentialWord.ToArray()) == word;
+            case Direction.Right:
+                if (column + ( word.Length - 1 ) >= crossword[row].Count) return false;
+                potentialWord = new List<char>();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    potentialWord.Add(crossword[row][column + i]);
+                }
+                return new String(potentialWord.ToArray()) == word;
+            case Direction.Left:
+                if (column - ( word.Length - 1 ) < 0) return false;
+                potentialWord = new List<char>();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    potentialWord.Add(crossword[row][column - i]);
+                }
+                return new String(potentialWord.ToArray()) == word;
+            case Direction.TopRight:
+                if (row - ( word.Length - 1 ) < 0 || column + ( word.Length - 1 ) >= crossword[row].Count) return false;
+                potentialWord = new List<char>();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    potentialWord.Add(crossword[row - i][column + i]);
+                }
+                return new String(potentialWord.ToArray()) == word;
+            case Direction.BottomRight:
+                if (row + ( word.Length - 1 ) >= crossword.Count || column + ( word.Length - 1 ) >= crossword[row].Count) return false;
+                potentialWord = new List<char>();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    potentialWord.Add(crossword[row + i][column + i]);
+                }
+                return new String(potentialWord.ToArray()) == word;
+            case Direction.BottomLeft:
+                if (row + ( word.Length - 1 ) >= crossword.Count || column - ( word.Length - 1 ) < 0) return false;
+                potentialWord = new List<char>();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    potentialWord.Add(crossword[row + i][column - i]);
+                }
+                return new String(potentialWord.ToArray()) == word;
+            case Direction.TopLeft:
+                if (row - ( word.Length - 1 ) < 0 || column - ( word.Length - 1 ) < 0) return false;
+                potentialWord = new List<char>();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    potentialWord.Add(crossword[row - i][column - i]);
+                }
+                return new String(potentialWord.ToArray()) == word;
+            default:
+                return false;
+        }
+    }
+
+    class MasMatch
+    {
+        private int topLeftRow;
+        private int topLeftColumn;
+        private int bottomRightRow;
+        private int bottomRightColumn;
+        public MasMatch(int topLeftRow, int topLeftColumn, int bottomRightRow, int bottomRightColumn) 
+        {
+            this.topLeftRow = topLeftRow;
+            this.topLeftColumn = topLeftColumn;
+            this.bottomRightRow = bottomRightRow;
+            this.bottomRightColumn = bottomRightColumn;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is MasMatch match) 
+            {
+                return topLeftColumn == match.topLeftColumn && topLeftRow == match.topLeftRow && bottomRightColumn == match.bottomRightColumn && bottomRightRow == match.bottomRightRow;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    static void dayFourPartTwo(StreamReader reader)
+    {
+        List<List<char>> crossword = new List<List<char>>();
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            crossword.Add(line.ToList());
+        }
+
+        int total = 0;
+        List<MasMatch> matches = new List<MasMatch>();
+        for (int i = 0; i < crossword.Count; i++)
+        {
+            for (int j = 0; j < crossword[i].Count; j++)
+            {
+                if (crossword[i][j] == 'M')
+                {
+                    string word = "MAS";
+                    if (hasWord(crossword, i, j, Direction.BottomLeft, word)) 
+                    {
+                        if (hasWord(crossword, i, j - (word.Length - 1), Direction.BottomRight, word))
+                        {
+                            MasMatch match = new MasMatch(i, j - (word.Length - 1), i + (word.Length - 1), j);
+                            if (!matches.Contains(match)) matches.Add(match);
+                        }
+                        if (hasWord(crossword, i + (word.Length - 1), j, Direction.TopLeft, word)) 
+                        {
+                            MasMatch match = new MasMatch(i, j - (word.Length - 1), i + (word.Length - 1), j);
+                            if (!matches.Contains(match)) matches.Add(match);
+                        }
+                    }
+                    if (hasWord(crossword, i, j, Direction.BottomRight, word))
+                    {
+                        if (hasWord(crossword, i - (word.Length - 1), j, Direction.TopRight, word))
+                        {
+                            MasMatch match = new MasMatch(i, j, i + (word.Length - 1), j + (word.Length - 1));
+                            if (!matches.Contains(match)) matches.Add(match);
+                        }
+                        if (hasWord(crossword, i, j + (word.Length - 1), Direction.BottomLeft, word))
+                        {
+                            MasMatch match = new MasMatch(i, j, i + (word.Length - 1), j + (word.Length - 1));
+                            if (!matches.Contains(match)) matches.Add(match);
+                        }
+                    }
+                    if (hasWord(crossword, i, j, Direction.TopLeft, word))
+                    {
+                        if (hasWord(crossword, i, j + (word.Length - 1), Direction.TopRight, word))
+                        {
+                            MasMatch match = new MasMatch(i - (word.Length - 1), j - (word.Length - 1), i, j);
+                            if (!matches.Contains(match)) matches.Add(match);
+                        }
+                        if (hasWord(crossword, i - (word.Length - 1), j, Direction.BottomLeft, word))
+                        {
+                            MasMatch match = new MasMatch(i - (word.Length - 1), j - (word.Length - 1), i, j);
+                            if (!matches.Contains(match)) matches.Add(match);
+                        }
+                    }
+                    if (hasWord(crossword, i, j, Direction.TopRight, word))
+                    {
+                        if (hasWord(crossword, i, j + (word.Length - 1), Direction.TopLeft, word))
+                        {
+                            MasMatch match = new MasMatch(i - (word.Length - 1), j, i, j + (word.Length - 1));
+                            if (!matches.Contains(match)) matches.Add(match);
+                        }
+                        if (hasWord(crossword, i - (word.Length - 1), j, Direction.BottomRight, word))
+                        {
+                            MasMatch match = new MasMatch(i - (word.Length - 1), j, i, j + (word.Length - 1));
+                            if (!matches.Contains(match)) matches.Add(match);
+                        }
+                    }
+                }
+            }
+        }
+        Console.WriteLine(matches.Count);
+    }
+
+    #endregion dayFour
 }
